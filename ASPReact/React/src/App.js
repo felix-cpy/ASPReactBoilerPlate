d8d03c30f1d4c588fd78378100732c964fd2608c
felix-cpy <felix.cpy@gmail.com>
2022-10-23 13:21:01 +0800

    init boiler plate code

---
+++ ASPReact/React/src/App.js
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import { Route, Routes,  BrowserRouter} from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            this is a react test element
+                <Routes>
+                    <Route path='/' element={<div>this is home stuff</div>} />
+                    <Route path='/test' element={<div>This is test stuff</div> } />
+                </Routes>
+                
+        </div>)
+
+}
+
+export default App;
---
+++ ASPReact/React/src/index.js
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+import {render} from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+
+import App from './App'
+
+var rootElem = document.getElementById('root');
+
+render(
+    <React.Fragment>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.Fragment>
+     , rootElem)
